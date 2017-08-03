c1e42184437e8992cbe670c508945e20993f9efa
Andrew Wright <prestoassai@hotmail.com>
2017-08-03 15:40:31 +0200

    cocktail app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,40 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:show, :new, :create]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.ingredient = Ingredient.find(params["dose"]["ingredient"])
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save!
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,28 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :cocktail, presence: true
   validates :ingredient, presence: true
-  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+
+  def index
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.ingredient = Ingredient.find_by_name(params[:ingredient])
+    @dose.save
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
+
 end
